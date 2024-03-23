b53568764a958dff7667456035eabe338eaae376
erstwhilest <themechanization@gmail.com>
2024-03-23 11:59:58 -0400

    custom icons working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import React, {useEffect} from 'react';
 import axios from 'axios';
 
+import Map from './Map'
+
 function App() {
 
   useEffect(() => {
@@ -18,20 +20,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map />
     </div>
   );
 }
