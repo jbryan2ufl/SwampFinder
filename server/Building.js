c3c45682bef49500307c29c7de4262dcae1f5dd2
Yuri Gelli <yuri.gelli@ufl.edu>
2024-03-23 11:26:03 -0400

    Basic classes added
    Also minor adjustment to README

---
+++ server/Building.js
@@ -0,0 +1,9 @@
+class Building{
+    constructor(name, latitude, longitude){
+        this.name = name;
+        this.classrooms = [];
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+    
+}
