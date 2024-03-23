dc146aef80887c18f90d88ac49500fe560406663
Yuri Gelli <yuri.gelli@ufl.edu>
2024-03-23 19:45:45 -0400

    Classroom data input finalization

--- server/ClassRoom.js
+++ server/ClassRoom.js
@@ -1,11 +1,16 @@
+const Building = require('./Building.js')
+
 class ClassRoom {
-    constructor(name){
+    constructor(name, room_number, availability_matrix){
         this.name = name;
-        this.availabilityMatrix = []
+        this.room_number = room_number;
+        this.availability_matrix = availability_matrix
     }
 
-    
+
 }
+
+module.exports = ClassRoom;
 
     
 
